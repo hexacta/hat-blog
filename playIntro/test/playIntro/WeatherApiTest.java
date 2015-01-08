86429e902c9cec22c9e9c2e0214fb14b3057cb9e
Marcos Ariel <mfoglino@gmail.com>
2015-01-08 16:47:13 -0300

    adds asserts

--- playIntro/test/playIntro/WeatherApiTest.java
+++ playIntro/test/playIntro/WeatherApiTest.java
@@ -5,6 +5,9 @@ import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 import play.libs.ws.WS;
@@ -22,7 +25,9 @@ public class WeatherApiTest {
 				String url = "http://localhost:9000/aTimeDemandingService";
 				JsonNode asJson = WS.url(url).get().get(5000L).asJson();
 
-				System.out.println(asJson);			
+				System.out.println(asJson);
+				
+				assertThat(asJson.toString(), containsString("Buenos Aires"));
 			}
 		});
     }
